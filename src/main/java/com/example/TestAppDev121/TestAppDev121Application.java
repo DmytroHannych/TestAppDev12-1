eccd5c8e2517aa4cf6c7d97aa71f728111f39c7e
Dmytro Hannych <you@example.com>
2023-06-22 22:36:24 +0300

    init

---
+++ src/main/java/com/example/TestAppDev121/TestAppDev121Application.java
@@ -0,0 +1,13 @@
+package com.example.TestAppDev121;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAppDev121Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAppDev121Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TestAppDev121/TestAppDev121ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestAppDev121;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAppDev121ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
